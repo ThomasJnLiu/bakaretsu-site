06cbcde21cc61a483287e0c784f82350d6089bf1
thomas <thomasjnliu@gmail.com>
2025-11-01 23:27:33 -0400

    add under construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import comicsInfo from "../public/comics/meta.json"
+import "./App.css"
 
 function App() {
   console.log(import.meta.url)
 
   return (
     <>
-      {comicsInfo.map(comic => {
+    <img src = "/underconstruction.png" style = {{width:"25%;"}} class = "under-construction-img" />
+      {/* {comicsInfo.map(comic => {
         return (<img key = {comic.id} src = {`/comics/${comic.name}`}/>)
-      })}
+      })} */}
     </>
   )
 }
